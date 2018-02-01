a3945550825c0ab590d45a14e553671a1534127c
Douglas <douglas@grupoartisan.com.br>
2018-02-01 13:12:14 -0200

    Atualizaçoes Recarga

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,14 +1,16 @@
 import { BoletosComponent } from './containers/boletos/boletos.component';
 import { RecargaComponent } from './containers/recarga/recarga.component';
+import { HomePageComponent } from './containers/home/home.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'boleto', pathMatch: 'full'},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'boleto',data: { page: 'one' }, component: BoletosComponent},
+  {path: 'home',data: { page: 'one' }, component: HomePageComponent},
   {path: 'recarga',data: { page: 'two' }, component: RecargaComponent}
 ];
-
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,19 +5,24 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { BoletosComponent } from './containers/boletos/boletos.component';
 import { RecargaComponent } from './containers/recarga/recarga.component';
+import { HomePageComponent } from './containers/home/home.component';
+
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { SweetAlert2Module } from '@toverux/ngx-sweetalert2';
+import { TextMaskModule } from 'angular2-text-mask';
 
 
 const COMPONENTS = [
   BoletosComponent,
-  RecargaComponent
+  RecargaComponent,
+  HomePageComponent
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     ModalModule,
+    TextMaskModule,
     HomeRoutingModule,
     SharedModule,
     SweetAlert2Module
